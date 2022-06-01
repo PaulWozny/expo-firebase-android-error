374171d34a7180c5a551b8086b9f2283672e3f40
Pawel Wozny <pawel.wozny@protonmail.com>
2022-06-01 22:00:19 +0200

    initial commit

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+// workaround for initializing firebase 
+// https://stackoverflow.com/questions/72158122/expo-firebase-authentication-while-trying-to-resolve-module-idb-from-file
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push("cjs");
+
+module.exports = defaultConfig;
